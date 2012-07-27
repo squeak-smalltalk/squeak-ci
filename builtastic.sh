359e019b69df5a1ec6c6d099b5f82a80dc20b4d8
Sean DeNigris <sean@clipperadams.com>
2012-07-27 09:19:30 -0300

    Use $WORKSPACE env var in lieu of hard-coded paths

--- builtastic.sh
+++ builtastic.sh
@@ -1,3 +1,3 @@
-exec /var/lib/jenkins/workspace/Squeak/coglinux/lib/squeak/4.0-2522/squeak -headless /var/lib/jenkins/workspace/Squeak/Squeak4.4-12053.image /var/lib/jenkins/workspace/Squeak/tests.st &
+exec "$WORKSPACE"/coglinux/lib/squeak/4.0-2522/squeak -headless /var/lib/jenkins/workspace/Squeak/Squeak4.4-12053.image "$WORKSPACE"/tests.st &
 
 exit 0
