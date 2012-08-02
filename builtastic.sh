6e1e25f0e11a2ce910fe58a17db2aaa7d1ae307e
Frank Shearar <frank@lshift.net>
2012-08-02 16:49:50 +0100

    Protect against spaces in the workspace name.

--- builtastic.sh
+++ builtastic.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 : ${WORKSPACE:=`pwd`} # Default to the current directory to ease testing
 
-mkdir $WORKSPACE/target/
-cp $WORKSPACE/Squeak4.4-12053.image $WORKSPACE/target/ImageUnderTest.image
-cp $WORKSPACE/Squeak4.4-12053.changes $WORKSPACE/target/ImageUnderTest.changes
+mkdir "$WORKSPACE/target/"
+cp "$WORKSPACE/Squeak4.4-12053.image" "$WORKSPACE/target/ImageUnderTest.image"
+cp "$WORKSPACE/Squeak4.4-12053.changes" "$WORKSPACE/target/ImageUnderTest.changes"
 
-$WORKSPACE/coglinux/bin/squeak -headless $WORKSPACE/target/ImageUnderTest.image $WORKSPACE/tests.st
+"$WORKSPACE/coglinux/bin/squeak" -headless "$WORKSPACE/target/ImageUnderTest.image" "$WORKSPACE/tests.st"
 
 exit 0
