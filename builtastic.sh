c53f0642f204e1719294de3c263d75b3cae119ad
Frank Shearar <frank@lshift.net>
2012-08-03 14:35:33 +0100

    Better names, let the test run status code bubble up.

--- builtastic.sh
+++ builtastic.sh
@@ -4,11 +4,11 @@
 IMAGE_NAME=Squeak4.4-trunk
 
 mkdir -p "$WORKSPACE/target/"
-cp "$WORKSPACE/$IMAGE_NAME.image" "$WORKSPACE/target/ImageUnderTest.image"
-cp "$WORKSPACE/$IMAGE_NAME.changes" "$WORKSPACE/target/ImageUnderTest.changes"
+cp "$WORKSPACE/$IMAGE_NAME.image" "$WORKSPACE/target/TrunkImage.image"
+cp "$WORKSPACE/$IMAGE_NAME.changes" "$WORKSPACE/target/TrunkImage.changes"
 cp "$WORKSPACE/SqueakV41.sources" "$WORKSPACE/target/SqueakV41.sources"
 cp "$WORKSPACE/HudsonBuildTools.st" "$WORKSPACE/target/HudsonBuildTools.st"
 
-"$WORKSPACE/coglinux/bin/squeak" -vm-sound-null -vm-display-null "$WORKSPACE/target/ImageUnderTest.image" "$WORKSPACE/tests.st"
+"$WORKSPACE/coglinux/bin/squeak" -vm-sound-null -vm-display-null "$WORKSPACE/target/TrunkImage.image" "$WORKSPACE/tests.st"
 
-exit 0
+#exit 0
