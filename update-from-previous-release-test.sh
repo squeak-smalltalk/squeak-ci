2017001171757a6a3590d01523c9da9e34952774
Frank Shearar <frank@lshift.net>
2012-08-03 14:35:04 +0100

    Basis for update-from-previous-release test. Not quite right yet.

---
+++ update-from-previous-release-test.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+: ${WORKSPACE:=`pwd`} # Default to the current directory to ease testing
+
+LAST_RELEASED_IMAGE_NAME=Squeak4.3
+
+mkdir -p "$WORKSPACE/target/"
+cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.image" "$WORKSPACE/target/UpdatedFromLastReleaseImage.image"
+cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.changes" "$WORKSPACE/target/UpdatedFromLastReleaseImage.changes"
+
+cp "$WORKSPACE/SqueakV41.sources" "$WORKSPACE/target/SqueakV41.sources"
+cp "$WORKSPACE/HudsonBuildTools.st" "$WORKSPACE/target/HudsonBuildTools.st"
+
+"$WORKSPACE/coglinux/bin/squeak" -vm-sound-null -vm-display-null "$WORKSPACE/target/UpdatedFromLastReleaseImage.image" "$WORKSPACE/tests.st"
