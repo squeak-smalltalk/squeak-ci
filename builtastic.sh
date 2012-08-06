189592ed98f405cb4c819fe338d6325ff219940d
Frank Shearar <frank@lshift.net>
2012-08-06 10:32:51 +0100

    Ensure that the clean, updated, image is not the image we keep: the image in which we run tests is thrown away.

--- builtastic.sh
+++ builtastic.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 : ${WORKSPACE:=`pwd`} # Default to the current directory to ease testing
 
-IMAGE_NAME=Squeak4.4-trunk
+TEST_IMAGE_NAME=Squeak4.4-trunk
+IMAGE_NAME="TrunkImage"
+VM="$WORKSPACE/coglinux/bin/squeak"
 
 mkdir -p "$WORKSPACE/target/"
-cp "$WORKSPACE/$IMAGE_NAME.image" "$WORKSPACE/target/TrunkImage.image"
-cp "$WORKSPACE/$IMAGE_NAME.changes" "$WORKSPACE/target/TrunkImage.changes"
+cp "$WORKSPACE/$TEST_IMAGE_NAME.image" "$WORKSPACE/target/$IMAGE_NAME.image"
+cp "$WORKSPACE/$TEST_IMAGE_NAME.changes" "$WORKSPACE/target/$IMAGE_NAME.changes"
 cp "$WORKSPACE/SqueakV41.sources" "$WORKSPACE/target/SqueakV41.sources"
 cp "$WORKSPACE/HudsonBuildTools.st" "$WORKSPACE/target/HudsonBuildTools.st"
 
-"$WORKSPACE/coglinux/bin/squeak" -vm-sound-null -vm-display-null "$WORKSPACE/target/TrunkImage.image" "$WORKSPACE/tests.st"
-
-#exit 0
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/update-image.st"
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/tests.st"
--- update-from-previous-release-test.sh
+++ update-from-previous-release-test.sh
@@ -2,12 +2,14 @@
 : ${WORKSPACE:=`pwd`} # Default to the current directory to ease testing
 
 LAST_RELEASED_IMAGE_NAME=Squeak4.3
+IMAGE_NAME="UpdatedFromLastReleaseImage"
+VM="$WORKSPACE/coglinux/bin/squeak"
 
 mkdir -p "$WORKSPACE/target/"
-cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.image" "$WORKSPACE/target/UpdatedFromLastReleaseImage.image"
-cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.changes" "$WORKSPACE/target/UpdatedFromLastReleaseImage.changes"
-
+cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.image" "$WORKSPACE/target/$IMAGE_NAME.image"
+cp "$WORKSPACE/$LAST_RELEASED_IMAGE_NAME.changes" "$WORKSPACE/target/$IMAGE_NAME.changes"
 cp "$WORKSPACE/SqueakV41.sources" "$WORKSPACE/target/SqueakV41.sources"
 cp "$WORKSPACE/HudsonBuildTools.st" "$WORKSPACE/target/HudsonBuildTools.st"
 
-"$WORKSPACE/coglinux/bin/squeak" -vm-sound-null -vm-display-null "$WORKSPACE/target/UpdatedFromLastReleaseImage.image" "$WORKSPACE/tests.st"
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/update-image.st"
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/tests.st"
