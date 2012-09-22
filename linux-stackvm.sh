328ae37f81da8918805af82d4b48f83df7735f2c
Frank Shearar <frank.shearar@gmail.com>
2012-09-22 21:28:31 +0100

    Jenkins can't seem to find some of these files.

--- linux-stackvm.sh
+++ linux-stackvm.sh
@@ -6,10 +6,10 @@ IMAGE_NAME="StackVM-TrunkImage"
 VM="$WORKSPACE/target/linux-stackvm/bin/squeak"
 
 mkdir -p "$WORKSPACE/target/"
-wget -O target/Squeak-4.10.2.2614-linux_x86_64.sh http://www.squeakvm.org/unix/release/Squeak-4.10.2.2614-linux_x86_64.sh
-chmod +x target/Squeak-4.10.2.2614-linux_x86_64.sh
-mkdir -p target/linux-stackvm
-target/Squeak-4.10.2.2614-linux_x86_64.sh -prefix=target/linux-stackvm
+wget -O "$WORKSPACE/target/Squeak-4.10.2.2614-linux_x86_64.sh" http://www.squeakvm.org/unix/release/Squeak-4.10.2.2614-linux_x86_64.sh
+chmod +x "$WORKSPACE/target/Squeak-4.10.2.2614-linux_x86_64.sh"
+mkdir -p "$WORKSPACE/target/linux-stackvm"
+"$WORKSPACE/target/Squeak-4.10.2.2614-linux_x86_64.sh" -prefix="$WORKSPACE/target/linux-stackvm"
 
 cp "$WORKSPACE/$TEST_IMAGE_NAME.image" "$WORKSPACE/target/$IMAGE_NAME.image"
 cp "$WORKSPACE/$TEST_IMAGE_NAME.changes" "$WORKSPACE/target/$IMAGE_NAME.changes"
