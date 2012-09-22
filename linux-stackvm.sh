115e4e97a06c8cbdd56de082a73aca26b295981b
Frank Shearar <frank.shearar@gmail.com>
2012-09-22 21:24:21 +0100

    Rebase commands relative to the root, like the other scripts.

--- linux-stackvm.sh
+++ linux-stackvm.sh
@@ -8,9 +8,8 @@ VM="$WORKSPACE/target/linux-stackvm/bin/squeak"
 mkdir -p "$WORKSPACE/target/"
 wget -O target/Squeak-4.10.2.2614-linux_x86_64.sh http://www.squeakvm.org/unix/release/Squeak-4.10.2.2614-linux_x86_64.sh
 chmod +x target/Squeak-4.10.2.2614-linux_x86_64.sh
-cd target
-mkdir -p linux-stackvm
-./Squeak-4.10.2.2614-linux_x86_64.sh -prefix=linux-stackvm
+mkdir -p target/linux-stackvm
+target/Squeak-4.10.2.2614-linux_x86_64.sh -prefix=target/linux-stackvm
 
 cp "$WORKSPACE/$TEST_IMAGE_NAME.image" "$WORKSPACE/target/$IMAGE_NAME.image"
 cp "$WORKSPACE/$TEST_IMAGE_NAME.changes" "$WORKSPACE/target/$IMAGE_NAME.changes"
