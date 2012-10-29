4aabb93d4712a5743e43ca8dd3a5e5089240f3ce
Frank Shearar <frank.shearar@gmail.com>
2012-10-29 10:03:08 +0000

    A special script for CentOS CI builds.

---
+++ build-local.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+# This script assumes a locally installed Squeak.
+# Its primary function is to allow CI to work on a CentOS box:
+# recent VMs can't build on CentOS because it uses an unsupported
+# glibc version.
+
+: ${WORKSPACE:=`pwd`} # Default to the current directory to ease testing
+
+TEST_IMAGE_NAME=Squeak4.4-trunk
+IMAGE_NAME="TrunkImage"
+VM="/usr/local/bin/squeak"
+
+mkdir -p "$WORKSPACE/target/"
+cp "$WORKSPACE/$TEST_IMAGE_NAME.image" "$WORKSPACE/target/$IMAGE_NAME.image"
+cp "$WORKSPACE/$TEST_IMAGE_NAME.changes" "$WORKSPACE/target/$IMAGE_NAME.changes"
+cp "$WORKSPACE/SqueakV41.sources" "$WORKSPACE/target/SqueakV41.sources"
+cp "$WORKSPACE/HudsonBuildTools.st" "$WORKSPACE/target/HudsonBuildTools.st"
+
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/update-image.st"
+$VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/tests.st"
