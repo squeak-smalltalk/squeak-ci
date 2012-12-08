0ab1d1f7c99215fa155393f06be639d79669f3d5
Frank Shearar <frank.shearar@gmail.com>
2012-12-08 20:29:32 +0000

    Thanks to David T. Lewis for the robustification.

--- builtastic.sh
+++ builtastic.sh
@@ -18,8 +18,23 @@ $VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORK
 
 # Run the image through an interpreter VM to make sure the image format is correct.
 
-if [-f $INTERPRETER_VM]; then
-  $INTERPRETER_VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/save-image.st"
+# Find a copy of the ckformat program, any one will do
+CKFORMAT=`find /usr/local/lib/squeak -name ckformat | head -1`
+
+if test -x "$CKFORMAT"; then
+ echo before format conversion: "$WORKSPACE/target/$IMAGE_NAME.image" image format `${CKFORMAT} "$WORKSPACE/target/$IMAGE_NAME.image"`
+else
+ echo WARNING: no ckformat found
+fi
+
+if test -f $INTERPRETER_VM; then
+ $INTERPRETER_VM -vm-sound-null -vm-display-null "$WORKSPACE/target/$IMAGE_NAME.image" "$WORKSPACE/save-image.st"
+else
+ echo WARNING: $INTERPRETER_VM not found, image not converted to format 6504
+fi
+
+if test -x "$CKFORMAT"; then
+ echo after format conversion: "$WORKSPACE/target/$IMAGE_NAME.image" image format `${CKFORMAT} "$WORKSPACE/target/$IMAGE_NAME.image"`
 fi
 
 # Copy the clean image so we can run the tests without touching the artifact.
