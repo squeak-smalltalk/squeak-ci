9ccd5202c22dc60382ec4c91a7a1a96f9233aec1
Frank Shearar <frank.shearar@gmail.com>
2012-12-23 18:08:53 +0000

    Point to the new Cog locationin target.

--- versions.sh
+++ versions.sh
@@ -2,8 +2,8 @@ SRC=$(cd $(dirname "$0"); pwd)
 
 INTERPRETER_VERSION=Squeak-4.10.2.2614
 COG_VERSION=2640
-INTERPRETER_VM=target/${INTERPRETER_VERSION}-src/bld/squeak
-COG_VM="${SRC}/cog.r${COG_VERSION}/coglinux/bin/squeak"
+INTERPRETER_VM="${SRC}/target/${INTERPRETER_VERSION}-src/bld/squeak"
+COG_VM="${SRC}/target/cog.r${COG_VERSION}/coglinux/bin/squeak"
 
 export INTERPRETER_VERSION
 export INTERPRETER_VM
