1731b80b8e2c79c8f79ec59a87008b7bcc0d5e47
Frank Shearar <frank.shearar@gmail.com>
2013-01-01 21:03:27 +0000

    Actually this doesn't build Cog. Right now it uses the Cog that's put on the squeakci.org server's location by CogVM. Later we'll do it 'properly'.

---
+++ build-local-cog.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# Run Trunk tests on a Trunk image running on top of a bleeding-edge Cog VM.
+# Assumes the existence of something named like Squeak-vm-unix-*-unofficial-src-*.tar.gz
+
+SRC=$(cd $(dirname "$0"); pwd)
+. "${SRC}/versions.sh"
+. "${SRC}/functions.sh"
+
+TEST_IMAGE_NAME="Squeak4.4-trunk"
+IMAGE_NAME="TrunkImage"
+COG_PATTERN="Squeak-vm-unix-*-unofficial-src"
+
+prepare_target ${SRC} $TEST_IMAGE_NAME $IMAGE_NAME
+
+curl -o "${SRC}/target/${IMAGE_NAME}.image" http://squeakci.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.image
+curl -o "${SRC}/target/${IMAGE_NAME}.changes" http://squeakci.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.changes
+curl -o "${SRC}/target/${IMAGE_NAME}.version" http://squeakci.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.version
+
+SQUEAK_UPDATE_NUMBER=`cat ${SRC}/target/TrunkImage.version`
+BASENAME=${SQUEAK_VERSION}-${SQUEAK_UPDATE_NUMBER}
+VM_TARBALL=`find target -name ${COG_PATTERN}-*.tar.gz | head -1`
+VM_VERSION=`find target -name ${COG_PATTERN} | head -1`
+
+#echo Unpacking Cog source...
+#(cd target; tar zxvf $VM_TARBALL)
+
+#VM="${SRC}/target/${VM_BASE}"
+VM=/var/lib/jenkins/workspace/CogVM/tmp/squeak
+
+echo Running ${BASENAME} tests on Cog ${VM_VERSION}...
+$VM -vm-sound-null -vm-display-null "${SRC}/target/$IMAGE_NAME.image" "${SRC}/tests.st"
