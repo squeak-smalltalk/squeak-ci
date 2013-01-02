b0907d03998c7ac6fcd8ae192726123b2c548967
Frank Shearar <frank.shearar@gmail.com>
2013-01-02 19:14:40 +0000

    Sigh. Untar the tarball.

--- build_interpreter_vm.sh
+++ build_interpreter_vm.sh
@@ -9,5 +9,6 @@ SRC=$(cd $(dirname "$0"); pwd)
 mkdir -p "${SRC}/target"
 TARBALL=`find . -name Squeak-vm-unix-*.tar.gz | head -1`
 mv $TARBALL "${SRC}/target/${TARBALL}"
+tar zxvf "${SRC}/target/${TARBALL}"
 SOURCE=`find target -name Squeak-vm-unix-*-src | head -1`
 (cd $SOURCE/platforms/unix; make)
