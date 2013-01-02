27576fd48c09af3dde75e1ac50049549bd0ff903
Frank Shearar <frank.shearar@gmail.com>
2013-01-02 21:07:24 +0000

    Cog's not built the same way as Interpreter.

--- build_cog_vm.sh
+++ build_cog_vm.sh
@@ -11,4 +11,6 @@ TARBALL=`find . -name Squeak-vm-unix-*.tar.gz | head -1`
 mv $TARBALL "${SRC}/target/${TARBALL}"
 (cd "${SRC}/target/"; tar zxvf ${TARBALL})
 SOURCE=`find target -name Squeak-vm-unix-*-src | head -1`
-(cd $SOURCE/platforms/unix; make)
+(cd $SOURCE/unixbuild/bld && \
+  ../../platforms/unix/config/configure --without-npsqueak CFLAGS="-g -O2 -msse2 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG -DITIMER_HEARTBEAT=1 -DNO_VM_PROFILE=1 -DCOGMTVM=0 -DDEBUGVM=0" LIBS=-lpthread && \
+  make install "${SRC}/target/vm/")
