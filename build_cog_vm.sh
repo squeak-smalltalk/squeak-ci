d5958139b7e06ddcc65410596c9e22d1233935f5
Frank Shearar <frank.shearar@gmail.com>
2013-01-02 20:36:57 +0000

    First attempt at something that can compile Cog on FreeBSD.

---
+++ build_cog_vm.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Assume that something's copied the source tarball to this directory.
+
+SRC=$(cd $(dirname "$0"); pwd)
+. "${SRC}/versions.sh"
+. "${SRC}/functions.sh"
+
+mkdir -p "${SRC}/target"
+TARBALL=`find . -name Squeak-vm-unix-*.tar.gz | head -1`
+mv $TARBALL "${SRC}/target/${TARBALL}"
+(cd "${SRC}/target/"; tar zxvf ${TARBALL})
+SOURCE=`find target -name Squeak-vm-unix-*-src | head -1`
+(cd $SOURCE/platforms/unix; make)
