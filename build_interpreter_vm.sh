356f91a219d9b6726de2e097f9a027b565ed82a7
Frank Shearar <frank.shearar@gmail.com>
2013-01-02 19:16:22 +0000

    Script of a thousand cuts: untar in the target dir.

--- build_interpreter_vm.sh
+++ build_interpreter_vm.sh
@@ -9,6 +9,6 @@ SRC=$(cd $(dirname "$0"); pwd)
 mkdir -p "${SRC}/target"
 TARBALL=`find . -name Squeak-vm-unix-*.tar.gz | head -1`
 mv $TARBALL "${SRC}/target/${TARBALL}"
-tar zxvf "${SRC}/target/${TARBALL}"
+(cd "${SRC}/target/"; tar zxvf ${TARBALL})
 SOURCE=`find target -name Squeak-vm-unix-*-src | head -1`
 (cd $SOURCE/platforms/unix; make)
