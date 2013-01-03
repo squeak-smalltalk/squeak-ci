5d8774c3a55d9a6702f1cb4350a74a64742e7e83
Frank Shearar <frank.shearar@gmail.com>
2013-01-03 12:56:03 +0000

    As it happens the only FreeBSD build slave (right now at least) is a 64-bit machine, and Cog is strictly 32-bit. Force this with -m32.

--- build_cog_vm.sh
+++ build_cog_vm.sh
@@ -12,5 +12,5 @@ mv $TARBALL "${SRC}/target/${TARBALL}"
 (cd "${SRC}/target/"; tar zxvf ${TARBALL})
 SOURCE=`find target -name Squeak-vm-unix-*-src | head -1`
 (cd $SOURCE/unixbuild/bld && \
-  ../../platforms/unix/config/configure --without-npsqueak CFLAGS="-g -O2 -msse2 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG -DITIMER_HEARTBEAT=1 -DNO_VM_PROFILE=1 -DCOGMTVM=0 -DDEBUGVM=0" LIBS=-lpthread && \
+  ../../platforms/unix/config/configure --without-npsqueak CFLAGS="-g -O2 -msse2 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG -DITIMER_HEARTBEAT=1 -DNO_VM_PROFILE=1 -DCOGMTVM=0 -DDEBUGVM=0 -m32" LIBS=-lpthread && \
   make install "${SRC}/target/vm/")
