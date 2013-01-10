4c03113b4ff3e211d596b47251a2602071554384
Frank Shearar <frank.shearar@gmail.com>
2013-01-10 21:17:40 +0000

    Don't try build the Cog VM if it happens to be in target.

--- build_interpreter_vm.sh
+++ build_interpreter_vm.sh
@@ -8,10 +8,10 @@ SRC=$(cd $(dirname "$0"); pwd)
 
 mkdir -p "${SRC}/target"
 curl -o "${SRC}/target/archive.zip" http://squeakci.org/job/InterpreterVM/lastSuccessfulBuild/artifact/*zip*/archive.zip
-pushd "${SRC}/target/"
+cd "${SRC}/target/"
 unzip -o archive.zip
 mv archive/* .
-TARBALL=`find . -name Squeak-vm-unix-*-src*.tar.gz | grep -v Cog | head -1`
+TARBALL=`find . -name 'Squeak-vm-unix-*-src*.tar.gz' | grep -v Cog | head -1`
 tar zxvf ${TARBALL}
-SOURCE=`find . -name Squeak-vm-unix-*-src | grep -v Cog | head -1`
+SOURCE=`find . -name 'Squeak-vm-unix-*-src' | grep -v Cog | head -1`
 (cd $SOURCE/platforms/unix; make)
