6761fb3815db6207310f10a8ab38672df0ed1c84
Frank Shearar <frank.shearar@gmail.com>
2013-02-12 10:24:12 +0000

    run-test needs to be able to bootstrap the running environment.

--- run-test.sh
+++ run-test.sh
@@ -19,6 +19,8 @@ mkdir -p "${SRC}/target"
 test ! -f "${SRC}/target/TrunkImage.image" && curl -sSo "${SRC}/target/TrunkImage.image" ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.image
 test ! -f "${SRC}/target/TrunkImage.changes" && curl -sSo "${SRC}/target/TrunkImage.changes" ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.changes
 test ! -f "${SRC}/target/TrunkImage.version" && curl -sSo "${SRC}/target/TrunkImage.version" ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.version
+test ! -f "${SRC}/target/SqueakV41.sources" && curl -sSo "${SRC}/target/SqueakV41.sources.gz" http://ftp.squeak.org/4.4/SqueakV41.sources.gz && gunzip "${SRC}/target/SqueakV41.sources.gz"
+cp HudsonBuildTools.st "${SRC}/target/"
 
 # Run the tests and snapshot the image post-test.
 echo Running tests on VM ${VM}...
