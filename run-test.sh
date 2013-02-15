110ff58ea541a5513b9a0e09726b07b7633d718f
Frank Shearar <frank.shearar@gmail.com>
2013-02-15 21:57:57 +0000

    Actually implement the don't-download-what-we-already-have.

--- run-test.sh
+++ run-test.sh
@@ -14,7 +14,6 @@ IMAGE_NAME="TrunkImage"
 fetch_cog_vm "linux"
 VM=$COG_VM
 
-echo Downloading a fresh Trunk image
 mkdir -p "${SRC}/target"
 
 if test -f "${SRC}/target/TrunkImage.version";
@@ -23,13 +22,16 @@ then
 else
     CURRENT_UPDATE=0
 fi
-LATEST_UPDATE=`curl -sS ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.version`
+`curl -sSo ${SRC}/target/TrunkImage.version ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.version`
+LATEST_UPDATE=`cat ${SRC}/target/TrunkImage.version`
 
 if test "x${CURRENT_UPDATE}x" != "x${LATEST_UPDATE}x";
 then
     echo Downloading Trunk version ${LATEST_UPDATE}
     curl -sSo "${SRC}/target/TrunkImage.image" ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.image
     curl -sSo "${SRC}/target/TrunkImage.changes" ${BASE_URL}/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.changes
+else
+    echo Reusing existing Trunk version ${LATEST_UPDATE}
 fi
 test ! -f "${SRC}/target/SqueakV41.sources" && curl -sSo "${SRC}/target/SqueakV41.sources.gz" http://ftp.squeak.org/4.4/SqueakV41.sources.gz && gunzip "${SRC}/target/SqueakV41.sources.gz"
 cp HudsonBuildTools.st "${SRC}/target/"
