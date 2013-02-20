7c5990a8182b0747fe45510e7e6774267a591b25
Frank Shearar <frank.shearar@gmail.com>
2013-02-20 21:45:05 +0000

    Fetch the source from the CogVM's last run.

--- build_cog_vm.sh
+++ build_cog_vm.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-# Assume that something's copied the source tarball to this directory.
-
 set -e
 
 SRC=$(cd $(dirname "$0"); pwd)
@@ -9,13 +7,12 @@ SRC=$(cd $(dirname "$0"); pwd)
 . "${SRC}/functions.sh"
 
 mkdir -p "${SRC}/target"
+curl -sS http://build.squeak.org/job/CogVM/99/artifact/*zip*/archive.zip > "${SRC}/target/archive.zip"
+(cd "${SRC}/target/"; unzip -o archive.zip)
 TARBALL=`find . -name Squeak-vm-unix-*.tar.gz | sort -r | head -1`
-if test "x${TARBALL}x" == "xx"; then
-    echo "Missing tarball!" && exit 1
-fi
-mv $TARBALL "${SRC}/target/${TARBALL}"
-(cd "${SRC}/target/"; tar zxvf ${TARBALL})
-SOURCE=`find target -name Squeak-vm-unix-*-src | sort -r | head -1`
-(cd $SOURCE/unixbuild/bld && \
+tar zxf ${TARBALL}
+SOURCE=`find . -name Squeak-vm-unix-*-src | sort -r | head -1`
+mv "${SOURCE}" "${SRC}/target/${SOURCE}"
+(cd ${SRC}/target/${SOURCE}/unixbuild/bld && \
   ../../platforms/unix/config/configure --without-npsqueak CFLAGS="-g -O2 -msse2 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG -DITIMER_HEARTBEAT=1 -DNO_VM_PROFILE=1 -DCOGMTVM=0 -DDEBUGVM=0 -m32" LIBS=-lpthread && \
   make install "${SRC}/target/vm/")
