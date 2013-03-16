bb5e67c48de16aa955d3cd6c85d74b3a88a6ac3d
Frank Shearar <frank.shearar@gmail.com>
2013-03-16 16:10:54 +0000

    Only move the image once something's made the target directory.

--- test/build_test.rb
+++ test/build_test.rb
@@ -6,11 +6,6 @@ describe "Trunk test suite" do
   RUN_TEST_IMAGE_NAME = "PostTestTrunkImage"
 
   before :all do
-    Dir.chdir(TARGET_DIR) {
-      # Copy the clean image so we can run the tests without touching the artifact.
-      FileUtils.cp("#{TRUNK_IMAGE}.image", "#{SRC}/target/#{RUN_TEST_IMAGE_NAME}.image")
-      FileUtils.cp("#{TRUNK_IMAGE}.changes", "#{SRC}/target/#{RUN_TEST_IMAGE_NAME}.changes")
-    }
     @os_name = identify_os
     @vm = case @os_name
           when "linux64"
@@ -18,6 +13,11 @@ describe "Trunk test suite" do
           else
             assert_cog_vm(@os_name)
           end
+    Dir.chdir(TARGET_DIR) {
+      # Copy the clean image so we can run the tests without touching the artifact.
+      FileUtils.cp("#{TRUNK_IMAGE}.image", "#{SRC}/target/#{RUN_TEST_IMAGE_NAME}.image")
+      FileUtils.cp("#{TRUNK_IMAGE}.changes", "#{SRC}/target/#{RUN_TEST_IMAGE_NAME}.changes")
+    }
   end
 
   after :all do
