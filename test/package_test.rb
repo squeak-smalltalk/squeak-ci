910ec4357c97edccbb93d0b1b8932c53911ce66d
Frank Shearar <frank.shearar@gmail.com>
2013-03-18 20:13:50 +0000

    Don't run on Squeak 4.3 for now: it doesn't have the SM whitespace-insensitive Installer.

--- test/package_test.rb
+++ test/package_test.rb
@@ -102,20 +102,20 @@ describe "External package on" do
     end
   end
 
-  context "Squeak 4.3" do
-    before :all do
-      squeak43_image = "Squeak4.3"
-      assert_target_dir
-      @os_name = identify_os
-      @cog_vm = assert_cog_vm(@os_name)
-      @interpreter_vm = assert_interpreter_vm(@os_name)
-      FileUtils.cp("#{squeak43_image}.image", "#{TARGET_DIR}/#{squeak43_image}.image")
-      FileUtils.cp("#{squeak43_image}.changes", "#{TARGET_DIR}/#{squeak43_image}.changes")
-      prepare_package_image(@os_name, squeak43_image, "update-squeak43-image.st")
-    end
+  # context "Squeak 4.3" do
+  #   before :all do
+  #     squeak43_image = "Squeak4.3"
+  #     assert_target_dir
+  #     @os_name = identify_os
+  #     @cog_vm = assert_cog_vm(@os_name)
+  #     @interpreter_vm = assert_interpreter_vm(@os_name)
+  #     FileUtils.cp("#{squeak43_image}.image", "#{TARGET_DIR}/#{squeak43_image}.image")
+  #     FileUtils.cp("#{squeak43_image}.changes", "#{TARGET_DIR}/#{squeak43_image}.changes")
+  #     prepare_package_image(@os_name, squeak43_image, "update-squeak43-image.st")
+  #   end
 
-    it_should_behave_like "all"
-  end
+  #   it_should_behave_like "all"
+  # end
 
   context "Squeak 4.4" do
     before :all do
