3db7f54b513110579efde3c421a9260fe9dde6a6
Frank Shearar <frank.shearar@gmail.com>
2013-03-27 10:01:29 +0000

    Now that 4.3 has a reasonably up to date Installer, run the various external packages against 4.3.

--- test/package_test.rb
+++ test/package_test.rb
@@ -138,23 +138,20 @@ describe "External package on" do
     end
   end
 
-  # The issue with Squeak 4.3 is that its Installer is a bit dated, and can't
-  # process versioned package names like "Control (1.2)" because it ends up
-  # looking for a package named "Control " (note the trailing whitespace).
-  # context "Squeak 4.3" do
-  #   before :all do
-  #     squeak43_image = "Squeak4.3"
-  #     assert_target_dir
-  #     @os_name = identify_os
-  #     @cog_vm = assert_cog_vm(@os_name)
-  #     @interpreter_vm = assert_interpreter_vm(@os_name)
-  #     FileUtils.cp("#{squeak43_image}.image", "#{TARGET_DIR}/#{squeak43_image}.image")
-  #     FileUtils.cp("#{squeak43_image}.changes", "#{TARGET_DIR}/#{squeak43_image}.changes")
-  #     prepare_package_image(@interpreter_vm, @os_name, squeak43_image, "update-squeak43-image.st")
-  #   end
+  context "Squeak 4.3" do
+    before :all do
+      squeak43_image = "Squeak4.3"
+      assert_target_dir
+      @os_name = identify_os
+      @cog_vm = assert_cog_vm(@os_name)
+      @interpreter_vm = assert_interpreter_vm(@os_name)
+      FileUtils.cp("#{squeak43_image}.image", "#{TARGET_DIR}/#{squeak43_image}.image")
+      FileUtils.cp("#{squeak43_image}.changes", "#{TARGET_DIR}/#{squeak43_image}.changes")
+      prepare_package_image(@interpreter_vm, @os_name, squeak43_image, "update-squeak43-image.st")
+    end
 
-  #   it_should_behave_like "all"
-  # end
+    it_should_behave_like "all"
+  end
 
   context "Squeak 4.4" do
     before :all do
