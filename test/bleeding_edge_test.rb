94513a3ed3a42844d509ebbc6029411ae43de905
Frank Shearar <frank.shearar@gmail.com>
2013-05-19 00:14:24 +0100

    Yet more bitrot in the bleeding edge tests.

--- test/bleeding_edge_test.rb
+++ test/bleeding_edge_test.rb
@@ -1,19 +1,21 @@
 require_relative 'test_helper'
 require_relative 'package_examples'
+require 'squeak-ci/test'
 require 'fileutils'
 require 'rspec'
 
 describe "External package in" do
   context "Squeak 4.5" do
     before :all do
-      squeak45_image = "Squeak4.5"
+      @base_image_name = "Squeak4.5"
       assert_target_dir
       @os_name = identify_os
       @cog_vm = assert_cog_vm(@os_name)
+      @cog_mt_vm = assert_cogmt_vm(@os_name)
       @interpreter_vm = assert_interpreter_vm(@os_name)
-      FileUtils.cp("#{squeak45_image}.image", "#{TARGET_DIR}/#{squeak45_image}.image")
-      FileUtils.cp("#{squeak45_image}.changes", "#{TARGET_DIR}/#{squeak45_image}.changes")
-      prepare_package_image(@interpreter_vm, @os_name, squeak45_image, "update-image.st")
+      FileUtils.cp("#{@base_image_name}.image", "#{TARGET_DIR}/#{@base_image_name}.image")
+      FileUtils.cp("#{@base_image_name}.changes", "#{TARGET_DIR}/#{@base_image_name}.changes")
+      prepare_package_image(@interpreter_vm, @os_name, @base_image_name, "update-image.st")
     end
 
     describe "Fuel (bleeding edge)" do
