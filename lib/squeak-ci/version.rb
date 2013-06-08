3aeacbf3e8109ab4d886988686d719537aa6f156
Frank Shearar <frank.shearar@gmail.com>
2013-06-08 20:55:29 +0100

    Fix #22: upgrade to Cog r.2732

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(13,13,2714).freeze
+COG_VERSION = CogVersion.new(13,22,2732).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
