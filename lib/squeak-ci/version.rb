860ace2d70801817c6ecd81c717cc412b8673c12
Frank Shearar <frank@lshift.net>
2013-07-16 11:12:42 +0100

    Update to Cog.r2749.

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(13,22,2732).freeze
+COG_VERSION = CogVersion.new(13,28,2749).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
