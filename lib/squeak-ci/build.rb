1645967493752636194e0c32a52e44810da2f4e6
Frank Shearar <frank.shearar@gmail.com>
2013-11-28 17:14:33 +0000

    Remove the debug-the-debugging stuff. Typo correction. Try visually delimit the debug info.

--- lib/squeak-ci/build.rb
+++ lib/squeak-ci/build.rb
@@ -277,16 +277,15 @@ def run_image_with_cmd(vm_name, arr_of_vm_args, image_name, cmd, timeout = 240)
       end
 
       if ! process_gone then
-        log("!!! Killed command #{cmd_count} for exceeding allotted time: nice #{base_cmd}.")
+        log("!!! Killing command #{cmd_count} for exceeding allotted time: nice #{base_cmd}.")
         # Dump out debug info from the image before we kill it. Don't use Process.kill
-        # bceause we want to capture stdout.
-        output = run_cmd("echo can you see this?")
-        $stdout.puts output
+        # because we want to capture stdout.
         output = run_cmd("kill -USR1 #{pid}")
         $stdout.puts output
         output = run_cmd("pstree #{pid}")
         $stdout.puts output
         Process.kill('KILL', pid)
+        log("!!! Killing command #{cmd_count}")
       end
     }
     Process.wait(pid)
