c7466c3880fdc66937a24085a5951961c98fb3e4
Frank Shearar <frank.shearar@gmail.com>
2014-01-24 21:33:36 +0000

    4.5 package builds

---
+++ test/squeak45_package_test.rb
@@ -0,0 +1,23 @@
+require_relative 'test_helper'
+require_relative 'package_examples'
+require 'fileutils'
+require 'rspec'
+require 'timeout'
+
+describe "External package in" do
+  context "Squeak 4.5" do
+    before :all do
+      @base_image_name = "Squeak4.5"
+      assert_target_dir
+      @os_name = identify_os
+      @cog_vm = assert_cog_vm(@os_name)
+      @cog_mt_vm = assert_cogmt_vm(@os_name)
+      @interpreter_vm = assert_interpreter_vm(@os_name)
+      FileUtils.cp("#{@base_image_name}.image", "#{TARGET_DIR}/#{@base_image_name}.image")
+      FileUtils.cp("#{@base_image_name}.changes", "#{TARGET_DIR}/#{@base_image_name}.changes")
+      prepare_package_image(@interpreter_vm, @os_name, @base_image_name, "update-squeak45-image.st")
+    end
+
+    it_behaves_like "external packages"
+  end
+end
