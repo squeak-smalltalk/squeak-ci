9611979a2bd5c2f44d077c0d162fa2f9a0c89626
Frank Shearar <frank.shearar@gmail.com>
2014-01-26 20:14:48 +0000

    Interpreter VMs don't have -reportheadroom, and sending squeak.sh the -version parameter fails anyway.

--- test/release_test.rb
+++ test/release_test.rb
@@ -28,13 +28,11 @@ describe "Release test suite" do
 
   context "image test suite" do
     it "should pass all tests on Interpreter VM", :interpreter => true do
-      Dir.chdir("#{SRC}/target") {
+      Dir.chdir(TARGET_DIR) {
         os_name = identify_os
         vm = assert_interpreter_vm(os_name)
         log("VM: #{vm}")
-        run_cmd("#{vm} -version")
         args = vm_args(os_name)
-        args << "-reportheadroom" unless os_name == "linux64"
         run_image_with_cmd(vm, vm_args(os_name), RUN_TEST_IMAGE_NAME, "#{SRC}/tests.st", 20.minutes)
       }
     end
