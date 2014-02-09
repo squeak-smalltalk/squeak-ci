a9a60de1aa8f3e474c6db31ba2c5bc4a02933249
Frank Shearar <frank.shearar@gmail.com>
2014-02-09 21:32:20 +0000

    SqueakTrunkPerformance job does benchmarking; no need to do that after the normal test suite.

--- test/image_test.rb
+++ test/image_test.rb
@@ -36,7 +36,6 @@ describe "Trunk test suite" do
         args = vm_args(@os_name)
         args << "-reportheadroom" unless @os_name == "linux64"
         run_image_with_cmd(@vm, vm_args(@os_name), RUN_TEST_IMAGE_NAME, "#{SRC}/tests.st", 20.minutes)
-        run_image_with_cmd(@vm, vm_args(@os_name), RUN_TEST_IMAGE_NAME, "#{SRC}/benchmarks.st", 10.minutes)
       }
     end
 
@@ -48,7 +47,6 @@ describe "Trunk test suite" do
         args = vm_args(@os_name)
         args << "-reportheadroom" unless @os_name == "linux64"
         run_image_with_cmd(vm, vm_args(@os_name), RUN_TEST_IMAGE_NAME, "#{SRC}/tests.st", 30.minutes)
-        run_image_with_cmd(@vm, vm_args(@os_name), RUN_TEST_IMAGE_NAME, "#{SRC}/benchmarks.st", 10.minutes)
       }
     end
   end
