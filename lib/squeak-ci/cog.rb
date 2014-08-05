37fb33509e3b74699282754908d9f5e5a8efeaf0
Frank Shearar <frank.shearar@gmail.com>
2014-08-05 21:47:39 +0100

    Let's try fix the MT/HT stuff

--- lib/squeak-ci/cog.rb
+++ lib/squeak-ci/cog.rb
@@ -21,14 +21,18 @@ class CogVersion
     basename = dir_name(os_name, vm_type)
     case os_name
     when "linux", "linux64" then
-      if vm_type == :spur then
+      if ht? vm_type then
         "#{basename}linuxht-#{version_string}.tgz" # Note the "ht" suffix
       else
         "#{basename}linux-#{version_string}.tgz"
       end
     when "windows" then "#{basename}win-#{version_string}.zip"
-    when "osx" then "#{basename}-#{version_string}.tgz"
+    when "osx"     then "#{basename}-#{version_string}.tgz"
     end
+  end
+
+  def ht? vm_type
+    [:spur, :mtht, :ht].include? vm_type
   end
 
   def lib_dir(base_path, os_name, vm_type = :normal)
@@ -46,13 +50,13 @@ class CogVersion
     base_name = dir_name(os_name, vm_type)
     case os_name
     when "linux", "linux64" then
-      if vm_type == :spur then
+      if ht? vm_type then
         base_path + "#{base_name}.r#{svnid}/#{base_name}linuxht/bin/squeak"
       else
         base_path + "#{base_name}.r#{svnid}/#{base_name}linux/bin/squeak"
       end
     when "windows" then base_path + "#{base_name}.r#{svnid}/#{base_name}win/SqueakConsole.exe"
-    when "osx" then base_path + "#{base_name}.r#{svnid}/#{base_name}/Contents/MacOS/Squeak"
+    when "osx"     then base_path + "#{base_name}.r#{svnid}/#{base_name}/Contents/MacOS/Squeak"
     else
       nil
     end
@@ -62,16 +66,17 @@ class CogVersion
     if os_name == "osx" then
       case vm_type
       when :normal then "Cog.app"
-      when :mt then "CogMT.app"
-      when :spur then "CogSpur.app"
+      when :mt     then "CogMT.app"
+      when :spur   then "CogSpur.app"
       else
         raise "Unknown vm_type #{vm_type.inspect} for 'osx' passed to CogVersion#dir_name"
       end
     else
       case vm_type
       when :normal then 'cog'
-      when :mt then 'cogmt'
-      when :spur then 'cogspur'
+      when :mt     then 'cogmt'
+      when :mtht   then 'cogmt'
+      when :spur   then 'cogspur'
       else
         raise "Unknown vm_type #{vm_type.inspect} for '#{os_name}' given to CogVersion#dir_name"
       end
