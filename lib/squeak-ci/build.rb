fe8d1f03638358c0a4ab99f34feae4906c1dea8f
Frank Shearar <frank.shearar@gmail.com>
2014-09-05 22:06:34 +0100

    Cog r.3063 doesn't ship with non-HT VMs.

--- lib/squeak-ci/build.rb
+++ lib/squeak-ci/build.rb
@@ -63,7 +63,10 @@ def assert_coglike_vm(os_name, vm_type)
 end
 
 def assert_cog_vm(os_name)
-  return assert_coglike_vm(os_name, :normal)
+  return case os_name
+         when "linux", "linux64" then assert_coglike_vm(os_name, :ht)
+         else assert_coglike_vm(os_name, :normal)
+         end
 end
 
 def assert_cogmt_vm(os_name)
