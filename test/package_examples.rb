36c410769eff71e8a4dd868905485fdc3af8ea11
Frank Shearar <frank.shearar@gmail.com>
2014-09-09 10:47:08 +0100

    Fix Interpreter package tests

--- test/package_examples.rb
+++ test/package_examples.rb
@@ -42,8 +42,8 @@ shared_examples "an external package" do
     end
 
     it "on Interpreter", :interpreter => true do
-      assert_interpreter_compatible_image(@interpreter_vm, image_name)
       with_copy(package, "interpreter") { | image_name |
+        assert_interpreter_compatible_image(@interpreter_vm, image_name)
         run_test_with_timeout(@interpreter_vm, @os_name, image_name, package, TEST_TIMEOUT)
       }
     end
