0d18d480c6b97670e0b49a7b708623da2625448e
Frank Shearar <frank.shearar@gmail.com>
2014-10-13 22:03:56 +0100

    Turns out Cog r.3097 was really just a Newspeak VM.

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(14,40,3097).freeze
+COG_VERSION = CogVersion.new(14,39,3095).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
