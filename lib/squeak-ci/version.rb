91d2b0e47ee1651e2d258abcb3d96c0a8c99f252
Frank Shearar <frank.shearar@gmail.com>
2014-10-14 08:44:48 +0100

    Upgrade to Cog r.3104 (which does have Squeak VMs this time).

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(14,39,3095).freeze
+COG_VERSION = CogVersion.new(14,41,3104).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
