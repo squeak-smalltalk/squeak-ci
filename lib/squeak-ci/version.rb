9caf7ec418ebfc228c343ffbe292cd28c7635700
Frank Shearar <frank.shearar@gmail.com>
2014-10-26 22:02:34 +0000

    Upgrade to Cog r.3114

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(14,41,3104).freeze
+COG_VERSION = CogVersion.new(14,42,3114).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
