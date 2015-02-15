af8ea6a43962611999826400464cf1c0e3046044
Frank Shearar <frank.shearar@gmail.com>
2015-02-15 21:15:37 +0000

    Revert to Cog r.3253, at Eliot's suggestion.

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,6 +1,6 @@
 require_relative 'cog'
 
-COG_VERSION = CogVersion.new(15,06,3254).freeze
+COG_VERSION = CogVersion.new(15,06,3253).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 
