af2b25e109d2867cdde07327158d943245350f0a
Tobias Pape <tobias@netshed.de>
2015-05-11 18:54:10 +0200

    Fix Cog verion nummer

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,7 +1,7 @@
 require_relative 'cog'
 
 #COG_VERSION = CogVersion.new(15,06,3253).freeze
-COG_VERSION = CogVersion.new(15,15,3332).freeze
+COG_VERSION = CogVersion.new(15,19,3332).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 MAC_INTERPRETER_VERSION="4.2.5beta1U"
