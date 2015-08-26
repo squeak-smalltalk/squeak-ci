bca7b5d8883a2ff93fd1487f6f9342df78d6de6b
Tobias Pape <tobias@netshed.de>
2015-08-26 13:32:09 +0200

    Bump Cog

--- lib/squeak-ci/version.rb
+++ lib/squeak-ci/version.rb
@@ -1,7 +1,7 @@
 require_relative 'cog'
 
 #COG_VERSION = CogVersion.new(15,06,3253).freeze
-COG_VERSION = CogVersion.new(15,28,3410).freeze
+COG_VERSION = CogVersion.new(15,33,3427).freeze
 INTERPRETER_VERSION="4.10.2.2614"
 WINDOWS_INTERPRETER_VERSION="4.10.2-2612"
 MAC_INTERPRETER_VERSION="4.2.5beta1U"
